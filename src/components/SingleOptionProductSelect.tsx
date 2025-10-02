acc9a60f78a54ee15686272b5f427d98e3549344
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-02 00:19:52 -0300

    feat: create single option value product for select in react select

---
+++ src/components/SingleOptionProductSelect.tsx
@@ -0,0 +1,21 @@
+import { components } from "react-select";
+import { type SingleValueProps } from "react-select";
+import type { OptionI } from "../config/MenuSelectStyles";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+
+export const CustomSingleValueProduct = (props: SingleValueProps<OptionI>) => {
+  const { label } = props.data;
+  return (
+    <components.SingleValue {...props} className="flex items-center gap-2">
+      <div className="flex items-center justify-between gap-2 flex-1">
+        <span className="text-[#2E3D50]">
+          {label}
+        </span>
+        <div>
+          <FaChevronUp size={10} className="text-[#029DF7]" />
+          <FaChevronDown size={10} className="text-[#029DF7]" />
+        </div>
+      </div>
+    </components.SingleValue>
+  );
+};
