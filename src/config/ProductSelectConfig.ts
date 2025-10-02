dd37bb5685c0d5c4d6c61d1597d2c2ef5b2dcd96
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-02 00:22:39 -0300

    feat: create new config for the options in react select

---
+++ src/config/ProductSelectConfig.ts
@@ -0,0 +1,15 @@
+interface optionsData {
+  value: string;
+  label: string;
+}
+
+export const optionsProductConfig: optionsData[] = [
+  {
+    value: "liminar",
+    label: "Liminar",
+  },
+  {
+    value: "comum",
+    label: "Comum",
+  },
+];
