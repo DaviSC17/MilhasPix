512a8e31ecaeb373370d92d8e752fac99ec5ea4b
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-02 00:19:17 -0300

    feat: create custom placeholder for cpfs select

---
+++ src/components/PlaceholderCpfsSelect.tsx
@@ -0,0 +1,15 @@
+import { components } from "react-select";
+import { type PlaceholderProps } from "react-select";
+import type { OptionI } from "../config/MenuSelectStyles";
+import { FiLock } from "react-icons/fi";
+
+export const CustomPlaceholderSelectCpfs = (props: PlaceholderProps<OptionI>) => {
+  return (
+    <components.Placeholder {...props} className="flex items-center gap-2">
+      <div className="flex items-center justify-between gap-2 flex-1">
+        <span>Ilimitados</span>
+        <FiLock size={20} color="#8F8F8F"/>
+      </div>
+    </components.Placeholder>
+  );
+};
