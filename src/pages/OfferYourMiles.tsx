6ec436c835af7b6e91806ac1024708e2e6c4dc40
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:32:02 -0300

    feat: create first version of Offer Your Miles page (mobile only yet)

---
+++ src/pages/OfferYourMiles.tsx
@@ -0,0 +1,9 @@
+import { FormYourMiles } from "../components/FormOfferYourMiles";
+
+export const OfferYourMiles = () => {
+  return (
+    <section className="p-4 flex flex-col gap-3 h-full md:px-10 max-md:max-w-2xl max-md:mx-auto relative">
+      <FormYourMiles />
+    </section>
+  );
+};
