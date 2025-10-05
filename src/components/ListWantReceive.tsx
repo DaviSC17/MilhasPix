3670874f3f88b2514a54a344dfa8222bd7bf2358
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 13:43:36 -0300

    refactor: change in the way the list works, now passing onChange and the value as parameters

--- src/components/ListWantReceive.tsx
+++ src/components/ListWantReceive.tsx
@@ -1,28 +1,41 @@
-import { useState } from "react";
+import { type ComponentProps } from "react";
 import { optionsWantToReceive } from "../config/WantToReceiveConfig";
 
-export const ListWantReceive = () => {
-  const [selected, setSelected] = useState<string>("");
+export const ListWantReceive = ({
+  value,
+  onChange,
+  ...props
+}: {
+  value: string;
+  onChange: (val: string) => void;
+} & ComponentProps<"ul">) => {
   return (
-    <ul className="flex flex-wrap gap-3 justify-center sm:max-w-full max-sm:max-w-96  self-center">
-      {optionsWantToReceive.map(({ label, value }) => (
-        <li key={value}>
+    <ul
+      {...props}
+      className="flex flex-wrap gap-3 justify-center sm:max-w-full max-sm:max-w-96 self-center"
+    >
+      {optionsWantToReceive.map(({ label, optionValue }) => (
+        <li key={optionValue}>
           <label
             tabIndex={0}
             onKeyDown={(e) => {
-              if (e.key === " " || e.key === "Enter") {
+              if (e.key === " ") {
                 e.preventDefault();
-                setSelected(value);
+                onChange(optionValue);
               }
             }}
-            className={`flex py-2 rounded-full border-2 text-black cursor-pointer  transition  duration-300 justify-center font-medium font-dm-sans leading-7  max-[450px]:w-34  md:w-40 w-42 ${selected === value ? "border-[#1E90FF]" : "border-[#F0F0F0] hover:border-[#1e8fff7e]"}`}
+            className={`flex py-2 rounded-full border-2 text-black cursor-pointer transition duration-300 justify-center font-medium font-dm-sans leading-7 max-[450px]:w-34 md:w-40 focus:outline-none focus:ring-2 focus:ring-[#1E90FF] w-42 ${
+              value === optionValue
+                ? "border-[#1E90FF]"
+                : "border-[#F0F0F0] hover:border-[#1e8fff7e]"
+            } ${props.className}`}
           >
             <input
               type="radio"
               name="term"
-              value={value}
-              checked={selected === value}
-              onChange={(e) => setSelected(e.target.value)}
+              value={optionValue}
+              checked={value === optionValue}
+              onChange={() => onChange(optionValue)}
               className="sr-only"
               tabIndex={-1}
             />
