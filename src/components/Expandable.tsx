7aeaa9f99301943beff3c9daaa38363b6c376839
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 13:25:55 -0300

    feat: add new component Expandable

--- src/components/Expandable.tsx
+++ src/components/Expandable.tsx
@@ -4,16 +4,17 @@ interface ExpandableI {
   title: string;
   Icon?: IconType;
   about?: string;
+  colorIcon: string;
 }
 
-export const Expandable = ({ title, about, Icon }: ExpandableI) => {
+export const Expandable = ({ title, about, Icon, colorIcon }: ExpandableI) => {
   return (
     <div className="p-3 border-1  border-[#E2E2E2]  rounded-lg flex flex-col  select-none gap-2 justify-center">
       <div className="flex items-center justify-between">
         <span className="font-dm-sans font-semibold leading-[130%] text-[#2E3D50]">
           {title}
         </span>
-        {Icon && <Icon size={24} color="#1E90FF" />}
+        {Icon && <Icon size={24} color={colorIcon} />}
       </div>
 
       {about && (
