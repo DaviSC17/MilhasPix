051bbd801be6d3601796871e48ddedc696cc2e99
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 13:32:13 -0300

    feat: add new structure (button) and new  value parameter

--- src/components/ContinueButton.tsx
+++ src/components/ContinueButton.tsx
@@ -1,19 +1,17 @@
-import { Link, type LinkProps } from "react-router";
 import { IoArrowForward } from "react-icons/io5";
 
-export const ContinueButton = ({ to, ...rest }: LinkProps) => {
+export const ContinueButton = ({ value }: { value: string }) => {
   return (
-    <Link
-      {...rest}
-      to={to}
-      className="flex items-center px-6 py-2.5 bg-[#1E90FF] w-fit rounded-4xl text-white gap-2 font-medium group active:scale-95 transition duration-300 hover:bg-[#1971ca]"
+    <button
+      type="submit"
+      className="flex items-center px-6 py-2.5 bg-[#1E90FF] w-fit rounded-4xl text-white gap-2 font-medium group active:scale-95 transition duration-300 hover:bg-[#1971ca] focus:outline-none focus:ring-2 focus:ring-[#0b4f94] cursor-pointer"
     >
-      Prosseguir
+      {value}
       <IoArrowForward
         color="white"
         size={20}
         className="mt-0.5 group-hover:animate-pulse"
       />
-    </Link>
+    </button>
   );
 };
