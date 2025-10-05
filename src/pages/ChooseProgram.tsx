736c9fd3c7ba2242a7fda9a6563c624a4e30b24c
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:31:18 -0300

    feat: create first version of Choose Program page (mobile only yet)

---
+++ src/pages/ChooseProgram.tsx
@@ -0,0 +1,9 @@
+import { FormChooseProgram } from "../components/FormChooseProgram";
+
+export const ChooseProgram = () => {
+  return (
+    <section className="p-4 flex flex-col gap-3 h-full  lg:px-20 md:max-w-4xl md:mx-auto lg:max-w-full">
+      <FormChooseProgram />
+    </section>
+  );
+};
