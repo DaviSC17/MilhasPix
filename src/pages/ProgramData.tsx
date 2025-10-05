d3cd9e0688e64b9ece040baca6756cd35ef88714
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:32:24 -0300

    feat: create first version o fProgram Data page (mobile only yet)

---
+++ src/pages/ProgramData.tsx
@@ -0,0 +1,10 @@
+import { FormProgramData } from "../components/FormProgramData";
+
+export const ProgramData = () => {
+  return (
+    <section className="p-4 flex flex-col gap-3 h-full md:px-10 max-md:max-w-2xl max-md:mx-auto ">
+      <FormProgramData />
+
+    </section>
+  );
+};
