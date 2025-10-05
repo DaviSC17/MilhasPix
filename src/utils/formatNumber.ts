b1b735c0f9562bcb8d46e4c0f644077fc73c6afd
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:46:57 -0300

    refactor: simplify createCurrencyFormatter function signature

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,20 +1,19 @@
 import type { Dispatch, SetStateAction } from "react";
 
-export const createCurrencyFormatter =
-  (setValue: Dispatch<SetStateAction<string>>) =>
-  (event: React.ChangeEvent<HTMLInputElement>) => {
-
-    const numericString = event.target.value.replace(/\D/g, "") || "0";
-    
-    const numericValue = parseInt(numericString, 10);
-
-    const formattedValue = (numericValue / 100).toLocaleString("pt-BR", {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-
-    setValue(formattedValue);
-  };
+export const createCurrencyFormatter = (
+  event: React.ChangeEvent<HTMLInputElement>,
+  onChange: (val: string) => void,
+  setValue: Dispatch<SetStateAction<string>>
+) => {
+  const numericString = event.target.value.replace(/\D/g, "") || "0";
+  const numericValue = parseInt(numericString, 10);
+  const formattedValue = (numericValue / 100).toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  setValue(formattedValue);
+  onChange(formattedValue);
+};
 
 export const valueForApi = (inputValue: string) => {
   return parseFloat(inputValue.replace(/\./g, "").replace(",", "."));
