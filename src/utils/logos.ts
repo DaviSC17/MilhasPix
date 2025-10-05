2ac45d1d1cdb279d0d45ea5a41abe1fa452377ad
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:48:13 -0300

    feat: create logosMap for airline logos

---
+++ src/utils/logos.ts
@@ -0,0 +1,11 @@
+import TudoAzulLogo from "../assets/TudoAzulLogo.svg";
+import SmilesLogo from "../assets/SmilesLogoListMyOffer.svg";
+import AirPortugalLogo from "../assets/AirPortugalLogo.svg";
+import LatamPassLogo from "../assets/LatamPassLogo.svg";
+
+export const logosMap: Record<string, string> = {
+  TudoAzul: TudoAzulLogo,
+  Smiles: SmilesLogo,
+  AirPortugal: AirPortugalLogo,
+  LatamPass: LatamPassLogo,
+};
