1dd96d4302ed4cc964d1ac14f88a4bc08804e77d
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 13:40:51 -0300

    styles: change some styles for better responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,17 @@ import logo from "../assets/Logo.svg";
 
 export const Header = () => {
   return (
-    <header className="w-full px-4 py-3.5 bg-[#1E90FF] flex justify-between lg:px-28">
-      <img src={logo} alt="Logo MilhasPix" />
+    <header className="w-full px-4 py-3.5 bg-[#1E90FF] flex justify-between">
+      <div className="flex justify-between w-full md:max-w-4xl md:mx-auto lg:max-w-full px-4">
+        <img src={logo} className="w-30" alt="Logo MilhasPix" />
 
-      <span
-        className="px-4 py-1.5 border-[#EAEAEA] font-dm-sans font-medium text-sm leading-6 border-[0.0625rem] rounded-full text-[#EAEAEA]"
-        aria-label="Saldo atual: R$ 282,12"
-      >
-        R$ 282,12
-      </span>
+        <span
+          className="px-4 py-1.5 border-[#EAEAEA] font-dm-sans font-medium text-sm leading-6 border-[0.0625rem] rounded-full text-[#EAEAEA]"
+          aria-label="Saldo atual: R$ 282,12"
+        >
+          R$ 282,12
+        </span>
+      </div>
     </header>
   );
 };
