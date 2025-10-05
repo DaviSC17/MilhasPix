23e0cae6166fee08691317ded905945e34f40037
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:38:54 -0300

    feat: create MyOfferI and MyOffersI interfaces for offer data

---
+++ src/types/GetMyOffersConfig.ts
@@ -0,0 +1,14 @@
+interface MyOfferI {
+  offerId: string;
+  offerStatus: string;
+  loyaltyProgram: string;
+  offerType: string;
+  accountLogin: string;
+  createdAt: string;
+  availableQuantity: number;
+}
+
+export interface MyOffersI {
+  totalQuantityOffers: number;
+  offers: MyOfferI[];
+}
