e38a15b7714349f22d635ccc2894877e27a0d292
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:19:27 -0300

    styles: add bew styles for error state and add cursor pointer

--- src/config/MenuSelectStyles.ts
+++ src/config/MenuSelectStyles.ts
@@ -1,25 +1,20 @@
 import type { StylesConfig } from "react-select";
+import type { OptionI } from "../types/MenuStyles";
 
-export interface OptionI {
-  value: string;
-  label: string;
-  alt?: string;
-  logo?: string;
-}
 
-export const MenuStyles: StylesConfig<OptionI, false> = {
+export const MenuStyles = (error: boolean): StylesConfig<OptionI, false> => ({
   control: (provided, state) => ({
     ...provided,
     borderRadius: "3.5rem",
     cursor: "pointer",
-    borderColor: state.isFocused ? "#1E90FF" : provided.borderColor,
-    borderWidth: 2,
-    padding: "0.375rem 0.625rem",
+    borderColor: error ? "#F87171" : state.isFocused ? "#1E90FF" : "#E2E2E2",
+    borderWidth: 1,
+    padding: "0.6rem 0.625rem",
     "&:hover": {
-      borderColor: "#1E90FF",
+      borderColor: error ? "#F87171" : "#1E90FF",
     },
     fontFamily: "DM Sans, sans-serif",
-    fontWeight: "600",
+    fontWeight: "500",
     fontSize: "0.875rem",
     lineHeight: "1.25rem",
   }),
@@ -27,6 +22,7 @@ export const MenuStyles: StylesConfig<OptionI, false> = {
   option: (provided, state) => ({
     ...provided,
     padding: "0.375rem 1.25rem",
+    cursor: "pointer",
     backgroundColor: state.isSelected ? "#1e8fff81" : provided.backgroundColor,
     "&:hover": {
       backgroundColor: state.isSelected ? "#1e8fff81" : "#1e8fff1f",
@@ -45,4 +41,9 @@ export const MenuStyles: StylesConfig<OptionI, false> = {
     fontSize: "0.875rem",
     lineHeight: "1.25rem",
   }),
-};
+
+  placeholder: (provided) => ({
+    ...provided,
+    color: error ? "#F87171" : provided.color,
+  }),
+});
