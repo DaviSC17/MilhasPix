a2a5e7965722c5ff9c3068e619b58f08cd74df35
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 14:20:20 -0300

    feat: create ReceiveUntil component

---
+++ src/components/ReceiveUntil.tsx
@@ -0,0 +1,11 @@
+export const ReceiveUntil = () => {
+  return (
+    <div className=" bg-[#12A19A1A]  w-full left-0 bottom-0 px-4.5 py-2 max-sm:-ml-4 max-sm:w-[calc(100%+2rem)] sm:rounded-full ">
+
+      <div className="text-[#12A19A] font-dm-sans flex justify-between font-medium leading-6.5">
+        <span>Receba até</span>
+        <span>R$24.325,23 </span>
+      </div>
+    </div>
+  );
+};
