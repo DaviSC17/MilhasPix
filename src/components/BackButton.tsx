b9ae726af7dd93c7d3674f2388a6fd5b647067a1
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 13:30:33 -0300

    feat: add a new structure type (button) and add navigate const

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,11 +1,14 @@
 import { IoArrowBack } from "react-icons/io5";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 export const BackButton = () => {
+  const navigate = useNavigate();
+
   return (
-    <Link
-      className="p-4 border border-[#E2E2E2] cursor-pointer rounded-full flex gap-2 items-center sm:px-6 sm:py-2.5 group hover:border-[#979797] transition duration-300 active:scale-95"
-      to={"/"}
+    <button
+      type="button"
+      onClick={() => navigate(-1)} 
+      className="p-4 border border-[#E2E2E2] cursor-pointer rounded-full flex gap-2 items-center sm:px-6 sm:py-2.5 group hover:border-[#979797] transition duration-300 active:scale-95 focus:outline-none focus:ring-2 focus:ring-[#1E90FF]"
     >
       <IoArrowBack
         color="#2E3D50"
@@ -15,6 +18,6 @@ export const BackButton = () => {
       <span className="hidden sm:flex text-[#2E3D50] text-sm font-medium">
         Voltar
       </span>
-    </Link>
+    </button>
   );
 };
