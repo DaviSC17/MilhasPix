3b774d326c3b1d8f93c700494517fca9d18af9e1
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-06 04:20:52 -0300

    feat: improve ReceiveUntil responsiveness and desktop styling

--- src/components/ReceiveUntil.tsx
+++ src/components/ReceiveUntil.tsx
@@ -1,10 +1,27 @@
-export const ReceiveUntil = () => {
-  return (
-    <div className=" bg-[#12A19A1A]  w-full left-0 bottom-0 px-4.5 py-2 max-sm:-ml-4 max-sm:w-[calc(100%+2rem)] sm:rounded-full ">
+import type { ComponentProps } from "react";
 
-      <div className="text-[#12A19A] font-dm-sans flex justify-between font-medium leading-6.5">
-        <span>Receba até</span>
-        <span>R$24.325,23 </span>
+interface ReceiveUntilI extends ComponentProps<"div"> {
+  titleClassName?: string;
+}
+
+export const ReceiveUntil = ({ titleClassName, ...props }: ReceiveUntilI) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <p
+        className={`hidden text-[#2E3D50] font-dm-sans font-medium leading-7 lg:flex ${titleClassName}`}
+      >
+        Receba até
+      </p>
+      <div
+        {...props}
+        className={`bg-[#12A19A1A] w-full px-4.5 py-2 max-sm:-ml-4 max-sm:w-[calc(100%+2rem)] sm:rounded-full ${props.className}`}
+      >
+        <div className="text-[#12A19A] font-dm-sans flex justify-between font-medium leading-6.5 w-full lg:text-[1.25rem]">
+          <span className="lg:hidden">Receba até</span>
+          <span className="lg:flex lg:w-full lg:justify-between">
+            <strong>R$</strong>24.325,23
+          </span>
+        </div>
       </div>
     </div>
   );
