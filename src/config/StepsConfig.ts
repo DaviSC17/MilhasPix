e5631043c9ba6dbcb167de31252205cbb43414f9
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-06 12:01:10 -0300

    feat: create steps data config for steps component

---
+++ src/config/StepsConfig.ts
@@ -0,0 +1,18 @@
+export const StepConfig = [
+  {
+    WhichStep: "Passo 1",
+    AboutStep: "Escolha a companhia aérea",
+  },
+  {
+    WhichStep: "Passo 2",
+    AboutStep: "Oferte suas milhas",
+  },
+  {
+    WhichStep: "Passo 3",
+    AboutStep: "Insira os dados do programa",
+  },
+  {
+    WhichStep: "Passo 4",
+    AboutStep: "Pedido finalizado",
+  },
+];
