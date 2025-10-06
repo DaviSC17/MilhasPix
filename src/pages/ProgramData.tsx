06bf2210df5411bf4d8eb2942bdd2cec6f0c61a6
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-06 05:32:32 -0300

    feat: add new Steps component and improve styles for better responsiveness

--- src/pages/ProgramData.tsx
+++ src/pages/ProgramData.tsx
@@ -1,8 +1,10 @@
 import { FormProgramData } from "../components/FormProgramData";
+import { Steps } from "../components/Steps";
 
 export const ProgramData = () => {
   return (
-    <section className="p-4 flex flex-col gap-3 h-full md:px-10 max-md:max-w-2xl max-md:mx-auto ">
+    <section className="p-4 flex flex-col gap-3 h-full md:px-10 max-md:max-w-2xl max-md:mx-auto lg:flex-row lg:h-fit lg:w-full lg:max-w-[86rem] lg:mx-auto">
+      <Steps />
       <FormProgramData />
 
     </section>
