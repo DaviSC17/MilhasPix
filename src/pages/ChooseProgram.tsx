366241145547ce4be7c57aea3b1ec93fbed8343f
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-06 01:56:51 -0300

    feat: add new Expandable component

--- src/pages/ChooseProgram.tsx
+++ src/pages/ChooseProgram.tsx
@@ -1,3 +1,5 @@
+import { FiMinus } from "react-icons/fi";
+import { Expandable } from "../components/Expandable";
 import { FormChooseProgram } from "../components/FormChooseProgram";
 import { Steps } from "../components/Steps";
 
@@ -6,6 +8,15 @@ export const ChooseProgram = () => {
     <section className="flex flex-col h-full  md:max-w-4xl md:mx-auto p-4 lg:max-w-[86rem] lg:flex-row lg:px-8 lg:py-8 lg:gap-4 xl:gap-2">
       <Steps />
       <FormChooseProgram />
+
+      <Expandable
+        colorIcon="#8F8F8F"
+        title="Selecione o Programa"
+        Icon={FiMinus}
+        className="hidden lg:p-3 lg:max-w-62 lg:h-fit  xl:flex"
+        about="Escolha de qual programa de fidelidade você quer vender suas milhas.
+                Use apenas contas em seu nome."
+      />
     </section>
   );
 };
