41783e0356e7436399b19f9c055cc63ff1bdfd7f
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-06 04:26:02 -0300

    feat: add new classNameTittle in interface

--- src/types/RankingConfig.ts
+++ src/types/RankingConfig.ts
@@ -1,3 +1,6 @@
-export interface RankingI {
+import type { ComponentProps } from "react";
+
+export interface RankingI extends ComponentProps<"ul"> {
   value: string;
+  classNameTitle?: string;
 }
