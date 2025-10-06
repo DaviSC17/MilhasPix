208ac5e4ee0e5e23473bb246cf4761da9e15a618
Davi Santanna <davicalvalcanti@gmail.com>
2025-10-05 21:52:44 -0300

    styles: improve on styles for better responsiveness

--- src/pages/ChooseProgram.tsx
+++ src/pages/ChooseProgram.tsx
@@ -1,8 +1,10 @@
 import { FormChooseProgram } from "../components/FormChooseProgram";
+import { Steps } from "../components/Steps";
 
 export const ChooseProgram = () => {
   return (
-    <section className="p-4 flex flex-col gap-3 h-full  lg:px-20 md:max-w-4xl md:mx-auto lg:max-w-full">
+    <section className="flex flex-col h-full  md:max-w-4xl md:mx-auto p-4 lg:max-w-[86rem] lg:flex-row lg:px-8 lg:py-8 lg:gap-4 xl:gap-2">
+      <Steps />
       <FormChooseProgram />
     </section>
   );
